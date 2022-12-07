5814c67d12f29def59a731a695545221f8870cf8
John Yeo <johnyeocx@gmail.com>
2022-12-07 00:44:31 +0000

    trying out env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = module.exports = {
+const nextConfig = {
+
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_PRIVATE_KEY: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY,
+    NEXT_PUBLIC_GOOGLE_SHEET_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID
+  },
   // serverRuntimeConfig: {
   //   API_URL: process.env.API_URL,
   // },
