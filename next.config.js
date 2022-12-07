ade50672d26d2b3b69a1ca3faaf0e02ee3482888
John Yeo <johnyeocx@gmail.com>
2022-12-07 00:41:09 +0000

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const nextConfig = module.exports = {
+  // serverRuntimeConfig: {
+  //   API_URL: process.env.API_URL,
+  // },
+  // Will be available on both server and client
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_PRIVATE_KEY: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY,
+    NEXT_PUBLIC_GOOGLE_SHEET_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID
+  }
 }
 
 module.exports = nextConfig
